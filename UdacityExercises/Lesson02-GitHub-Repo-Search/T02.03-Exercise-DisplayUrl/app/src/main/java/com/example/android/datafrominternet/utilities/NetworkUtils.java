cffb4971fdb269f14795828d00dc452f36596537
Albert Bustamante <albertbustamante@hotmail.com>
2018-01-16 12:23:21 -0600

    Exercises - Lesson 2 Exercise 3 - COMPLETE - Added buildUrlMethod to NetworkUtils, created makeGithubSearchQuery method, and updated search selected to call makeGithubSearchQuery

--- UdacityExercises/Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ UdacityExercises/Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -26,11 +29,8 @@ import java.util.Scanner;
  */
 public class NetworkUtils {
 
-    final static String GITHUB_BASE_URL =
-            "https://api.github.com/search/repositories";
-
+    final static String GITHUB_BASE_URL = "https://api.github.com/search/repositories";
     final static String PARAM_QUERY = "q";
-
     /*
      * The sort field. One of stars, forks, or updated.
      * Default: results are sorted by best match if no field is specified.
@@ -45,8 +45,18 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+        // COMPLETE (1) Fill in this method to build the proper Github query URL
+        Uri buildUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY,githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+        URL url = null;
+        try{
+            url = new URL(buildUri.toString());
+        }catch(MalformedURLException e){
+            e.printStackTrace();
+        }
+        return url;
     }
 
     /**
