cf34559c69cd41c4c4f34aa92284bf2bc5ccf2ab
Albert Bustamante <albertbustamante@hotmail.com>
2018-01-23 13:44:09 -0600

    Exercises: Lesson 5a Exercise 3: COMPLETE - Updated onCreate, onDestroy, and onSaveInstanceState.

--- UdacityExercises/Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifeCycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ UdacityExercises/Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifeCycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +40,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // COMPLETE (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static ArrayList<String> mLifecycleCallbacks = null;
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,10 +73,12 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
-
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
-
+        // COMPLETE (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        for(int i = mLifecycleCallbacks.size() -1; i >= 0; i--){
+            mLifecycleDisplay.append(mLifecycleCallbacks.get(i) + "\n");
+        }
+        // COMPLETE (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
         logAndAppend(ON_CREATE);
     }
 
@@ -132,9 +136,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        // COMPLETE (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -159,9 +162,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        // COMPLETE (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
