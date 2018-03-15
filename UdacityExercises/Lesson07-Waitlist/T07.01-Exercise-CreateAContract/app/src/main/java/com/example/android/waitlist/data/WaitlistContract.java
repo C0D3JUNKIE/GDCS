e8eef32b663ffa2eece1b739075f6295fc65eb18
Albert Bustamante <albertbustamante@hotmail.com>
2018-03-15 09:10:49 -0500

    Lesson 7 - Exercise 1 - COMPLETE: Added contract and updated gradle to 7.26

--- UdacityExercises/Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ UdacityExercises/Lesson07-Waitlist/T07.01-Exercise-CreateAContract/app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -6,12 +6,20 @@ public class WaitlistContract {
 
 
 
-    // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-
-        // TODO (2) Inside create a static final members for the table name and each of the db columns
+    // COMPLETE (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
+        // COMPLETE (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
 
+    public static final class WaitlistEntry implements BaseColumns{
+
+      public static final String WAITLIST_TABLE = "waitlist";
+      public static final String COLUMN_GUESTNAME = "guestName";
+      public static final String COLUMN_PARTYSIZE = "partySize";
+      public static final String COLUMN_TIMESTAMP = "timestamp";
+      public static final String COLUMN_ID = "index";
+
+    }
 }
