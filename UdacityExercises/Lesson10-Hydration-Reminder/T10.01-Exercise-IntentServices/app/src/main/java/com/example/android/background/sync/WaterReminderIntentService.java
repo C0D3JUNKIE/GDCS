cd2b80cb005862f7058199cf4597bfe7cce5c734
Albert Bustamante <albertbustamante@hotmail.com>
2018-03-28 15:47:09 -0500

    Exercises: Lesson 10 Exercise 1: COMPLETE - Created WaterReminderIntentService and ReminderTasks classes and updated MainActivity class incrementWater method.

--- UdacityExercises/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ UdacityExercises/Lesson10-Hydration-Reminder/T10.01-Exercise-IntentServices/app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -13,11 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.background.sync;
 
-// TODO (9) Create WaterReminderIntentService and extend it from IntentService
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import com.example.android.background.sync.ReminderTasks;
 
-//  TODO (10) Create a default constructor that calls super with the name of this class
+// COMPLETE (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+//  COMPLETE (10) Create a default constructor that calls super with the name of this class
+//  COMPLETE (11) Override onHandleIntent
+//      COMPLETE (12) Get the action from the Intent that started this Service
+//      COMPLETE (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+  public WaterReminderIntentService(){
+    super("WaterReminderIntentService");
+  }
 
-//  TODO (11) Override onHandleIntent
-//      TODO (12) Get the action from the Intent that started this Service
-//      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+  @Override
+  protected void onHandleIntent(@Nullable Intent intent) {
+    String action = intent.getAction();
+    ReminderTasks.executeTask(this, action);
+  }
+}
